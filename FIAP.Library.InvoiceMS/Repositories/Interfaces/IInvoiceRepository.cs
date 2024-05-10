d5444bc07651ae883c67ca624b7514b1f86d6009
demetrios arghirachis <dpa0702@gmail.com>
2024-05-10 16:47:31 -0300

    Retirando projeto desnecessário.

--- FIAP.Library.InvoiceMS/Repositories/Interfaces/IInvoiceRepository.cs
+++
@@ -1,9 +0,0 @@
-﻿using FIAP.Library.Domain.Entities;
-using FIAP.Library.Domain.Repository.Interfaces;
-
-namespace FIAP.Library.InvoiceMS.Repositories.Interfaces
-{
-    public interface IInvoiceRepository : IRepository<Invoice>
-    {
-    }
-}
